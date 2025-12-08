a3795d17ff168032c04204b58bb0287611144374
Motahareh Molaei <motahareh_molaei@yahoo.de>
2025-12-08 22:21:57 +0100

    create product class

---
+++ product_model.py
@@ -0,0 +1,9 @@
+class Product:
+    def __init__(self, id, product, brand, number_of_product, expire_date, price, total_price):
+        self.id = id
+        self.product = product
+        self.brand = brand
+        self.number_of_product = number_of_product
+        self.expire_date = expire_date
+        self.price = price
+        self.total_price = total_price
